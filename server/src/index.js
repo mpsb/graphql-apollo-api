9df7f954cf9f96cb6fd41db4ba50befdbc6db60c
mpsb <mps.bilo@gmail.com>
2022-04-13 06:19:07 +0800

    init commit

---
+++ server/src/index.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const { createStore } = require('./utils');
+const resolvers = require('./resolvers');
+
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const isEmail = require('isemail');
+
+const store = createStore();
+
+const server = new ApolloServer({ 
+    typeDefs,
+    resolvers,
+    dataSources: () => ({
+        launchAPI: new LaunchAPI(),
+        userAPI: new UserAPI({ store }),
+    }),
+    context: async ({ req }) => {
+        const auth = req.headers && req.headers.authorization || '';
+        const email = Buffer.from(auth, 'base64').toString('ascii');
+
+        if(!isEmail.validate(email)) {
+            return {user: null};
+        }
+
+        const users = await store.users.findOrCreate({where: email});
+        const user = users && user[0] || null;
+
+        return {user: {...user.dataValues}};
+    }
+});
+
+server.listen().then(() => {
+    console.log(`
+        Server is running!
+        Listening on port 4000
+    `)
+})
